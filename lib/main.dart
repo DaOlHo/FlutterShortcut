228a81765ce4834b670872183f3ecb7ac4c03497
Danole <44514463+DaOlHo@users.noreply.github.com>
2020-02-15 20:06:08 -0800

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ping/flutter_ping.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void ping() async {
+    try {
+      await FlutterPing.pingURL(url: "8.8.8.8");
+      await launch('https://yahoo.com', forceWebView: true);
+    } catch (e) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Error"),
+              content: Text("No Network"),
+            );
+          });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ping();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset("imgs/kiwi.jpg"),
+          ],
+        ),
+      ),
+    );
+  }
+}
